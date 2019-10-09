a3d960b742c668c89d5f699a5dbad2abd46d4f31
sriharsha1507 <sriharsha1507@gmail.com>
2019-10-08 23:25:32 -0500

    Added foreground notification for oreo or greater devices

--- android/app/src/main/kotlin/com/example/flutter_android_pet_tracking_background_service/tracking/service/PetTrackingService.kt
+++ android/app/src/main/kotlin/com/example/flutter_android_pet_tracking_background_service/tracking/service/PetTrackingService.kt
@@ -1,13 +1,25 @@
 package com.example.flutter_android_pet_tracking_background_service.tracking.service
 
+import android.app.Notification
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import com.example.flutter_android_pet_tracking_background_service.R
+import com.example.flutter_android_pet_tracking_background_service.utils.VersionChecker
 
 class PetTrackingService : Service() {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (VersionChecker.isGreaterThanOrEqualToOreo()) {
+            val builder: Notification.Builder =
+                    Notification.Builder(this, "sriharsha")
+                            .setContentText("Pet tracking mode")
+                            .setContentTitle("Background service")
+                            .setSmallIcon(R.drawable.launch_background)
+            startForeground(143, builder.build())
+        }
     }
 
     override fun onBind(intent: Intent): IBinder {
