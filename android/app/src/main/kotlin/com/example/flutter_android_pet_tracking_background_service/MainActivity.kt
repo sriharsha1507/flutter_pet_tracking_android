6c2bf698c45c10806fff15813ccdf1d4b7276114
sriharsha1507 <sriharsha1507@gmail.com>
2019-10-08 23:38:01 -0500

    Added helper function to start pet tracking service

--- android/app/src/main/kotlin/com/example/flutter_android_pet_tracking_background_service/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_android_pet_tracking_background_service/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.example.flutter_android_pet_tracking_background_service
 
+import android.content.Intent
 import android.os.Bundle
+import com.example.flutter_android_pet_tracking_background_service.tracking.service.PetTrackingService
+import com.example.flutter_android_pet_tracking_background_service.utils.VersionChecker
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+
+
+    }
+
+    private fun startPetTrackingService() {
+        val intent = Intent(this, PetTrackingService::class.java)
+
+        if (VersionChecker.isGreaterThanOrEqualToOreo()) {
+            startForegroundService(intent)
+        } else {
+            startService(intent)
+        }
+    }
 }
