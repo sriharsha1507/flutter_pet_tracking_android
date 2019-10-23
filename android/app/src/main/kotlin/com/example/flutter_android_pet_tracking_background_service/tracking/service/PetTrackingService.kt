18afb0e3014dcb4c79d276d21f2baf738869db2d
sriharsha1507 <sriharsha1507@gmail.com>
2019-10-22 22:41:42 -0500

    Added location Manager configuration done for starting pet tracking

--- android/app/src/main/kotlin/com/example/flutter_android_pet_tracking_background_service/tracking/service/PetTrackingService.kt
+++ android/app/src/main/kotlin/com/example/flutter_android_pet_tracking_background_service/tracking/service/PetTrackingService.kt
@@ -1,24 +1,40 @@
 package com.example.flutter_android_pet_tracking_background_service.tracking.service
 
+import android.Manifest
 import android.app.Notification
-import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Binder
+import android.os.Build
+import android.os.Bundle
 import android.os.IBinder
+import android.util.Log
 import com.example.flutter_android_pet_tracking_background_service.R
 import com.example.flutter_android_pet_tracking_background_service.utils.VersionChecker
 
-class PetTrackingService : Service(), TrackingService {
+private const val GPS_TRACKING_IN_MILLIS: Long = 1000
+private const val GPS_TRACKING_IN_DISTANCE_METERS: Float = 1f
+
+class PetTrackingService : Service(), TrackingService, LocationListener {
     private var listener: PetTrackingListener? = null
+    private lateinit var locationManager: LocationManager
+
+    override fun onCreate() {
+        super.onCreate()
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    }
 
     override fun onBind(intent: Intent): IBinder = LocalBinder()
 
     override fun start() {
         startForegroundNotification()
-        for (i in 0..10){
-            listener?.testData("Index $i")
-        }
+        initPetLocationTracking()
     }
 
     override fun stop() {
@@ -30,6 +46,22 @@ class PetTrackingService : Service(), TrackingService {
         this.listener = listener
     }
 
+    override fun onLocationChanged(location: Location) {
+        Log.e("GUNDALOCATION", "latitude-${location.latitude} && longitude-${location.longitude}")
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderEnabled(provider: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderDisabled(provider: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     private fun startForegroundNotification() {
         if (VersionChecker.isGreaterThanOrEqualToOreo()) {
             val builder: Notification.Builder =
@@ -39,6 +71,33 @@ class PetTrackingService : Service(), TrackingService {
                             .setSmallIcon(R.drawable.launch_background)
             startForeground(143, builder.build())
         }
+    }
+
+    private fun getLocationCriteria(): Criteria {
+        val criteria = Criteria()
+        criteria.accuracy = Criteria.ACCURACY_FINE
+        criteria.powerRequirement = Criteria.POWER_HIGH
+        criteria.isAltitudeRequired = false
+        criteria.isSpeedRequired = false
+        criteria.isCostAllowed = true
+        criteria.isBearingRequired = false
+        criteria.horizontalAccuracy = Criteria.ACCURACY_HIGH
+        criteria.verticalAccuracy = Criteria.ACCURACY_HIGH
+        return criteria
+    }
+
+    private fun initPetLocationTracking() {
+        if (if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                } else {
+                    TODO("VERSION.SDK_INT < M")
+                }) return
+        locationManager.requestLocationUpdates(
+                GPS_TRACKING_IN_MILLIS,
+                GPS_TRACKING_IN_DISTANCE_METERS,
+                getLocationCriteria(),
+                this,
+                null)
     }
 
     inner class LocalBinder : Binder() {
